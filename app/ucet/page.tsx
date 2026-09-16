fe226ebdca74f7353211c3a33b3966b0a6549468
wampirovka <github@paiskr.eu>
2026-09-17 00:22:01 +0200

    feat: add account route

---
+++ app/ucet/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function UcetPage() {
+  return <PlaceholderPage title="Můj účet" description="Osobní údaje, přístup a nastavení uživatelského účtu." />;
+}
