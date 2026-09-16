8cb92a77c811bf086060b6722297647d81fc8ef6
wampirovka <github@paiskr.eu>
2026-09-17 00:21:55 +0200

    feat: add reports route

---
+++ app/prehledy/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function PrehledyPage() {
+  return <PlaceholderPage title="Přehledy" description="Souhrny fakturace, úhrad a pohybů v pokladně." />;
+}
