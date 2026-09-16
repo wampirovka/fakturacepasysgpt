bad5530a881c01f5a1480e309cd4e552fb397d60
wampirovka <github@paiskr.eu>
2026-09-17 00:21:57 +0200

    feat: add settings route

---
+++ app/nastaveni/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function NastaveniPage() {
+  return <PlaceholderPage title="Nastavení" description="Údaje firmy, číselné řady, DPH, bankovní účty a výchozí hodnoty." />;
+}
