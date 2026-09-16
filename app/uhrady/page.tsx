8a639b2ee0d98e16a8f3bca38b247bb659536b9b
wampirovka <github@paiskr.eu>
2026-09-17 00:21:47 +0200

    feat: add payments route

---
+++ app/uhrady/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function UhradyPage() {
+  return <PlaceholderPage title="Úhrady" description="Centrální evidence plateb, částečných úhrad a způsobů platby." />;
+}
