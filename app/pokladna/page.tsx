8073c668842786d116c76b8945138ff995133947
wampirovka <github@paiskr.eu>
2026-09-17 00:21:49 +0200

    feat: add cash register route

---
+++ app/pokladna/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function PokladnaPage() {
+  return <PlaceholderPage title="Pokladna" description="Příjmy, výdaje, pokladní doklady a aktuální stav hotovosti." />;
+}
