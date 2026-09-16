b6fe2d8106bfb3313301564f3caa3982e066cfe4
wampirovka <github@paiskr.eu>
2026-09-17 00:21:52 +0200

    feat: add customers route

---
+++ app/zakaznici/page.tsx
@@ -0,0 +1,5 @@
+import { PlaceholderPage } from "@/components/placeholder-page";
+
+export default function ZakazniciPage() {
+  return <PlaceholderPage title="Zákazníci" description="Kontakty, fakturační údaje a historie dokladů zákazníků." />;
+}
